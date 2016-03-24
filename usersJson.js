9875d6724b1ab76671e6249ec3d86aeecea63a49
yGazhala <yuriy.gazhala@gmail.com>
2016-03-24 18:10:10 +0300

    first init

---
+++ usersJson.js
@@ -0,0 +1,34 @@
+'use strict';
+// This file was uploaded to firebase:
+// https://gazhala.firebaseio.com/contacts.json
+// I left it here, just for example
+
+var usersJson = `[
+    {
+        "id": 1,
+        "fullName": "Иванов Иван",
+        "avatarUrl": "https://randomuser.me/api/portraits/thumb/men/57.jpg",
+        "birthdate": "1976-10-10",
+        "gender": "мужской",
+        "address": "ул. Звенигороская, 47б",
+        "email": "ivanov@mail.ru"
+    },
+    {
+        "id": 2,
+        "fullName": "Петров Петр",
+        "avatarUrl": "https://randomuser.me/api/portraits/thumb/men/8.jpg",
+        "birthdate": "1957-01-14",
+        "gender": "мужской",
+        "address": "Пр. Науки, 13",
+        "email": "petrov@mail.ru"
+    },
+    {
+        "id": 3,
+        "fullName": "Натальина Наталья",
+        "avatarUrl": "https://randomuser.me/api/portraits/thumb/women/7.jpg",
+        "birthdate": "1990-07-03",
+        "gender": "женский",
+        "address": "ул. Феодоры, 59",
+        "email": "natasha@mail.ru"
+    }
+]`;
