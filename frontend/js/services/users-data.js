822de7e9f8a1107503f1b9ced0910896ca2fce15
yGazhala <yuriy.gazhala@gmail.com>
2016-06-07 13:04:40 +0300

    New content was added

--- frontend/js/services/users-data.js
+++ frontend/js/services/users-data.js
@@ -1,34 +1,78 @@
 'use strict';
 // This file was uploaded to firebase:
 // https://gazhala.firebaseio.com/contacts.json
-// I left it here, just for example
+// I left it here, just for example.
 
 var usersJson = `[
     {
-        "id": 1,
+        "id": 0,
         "fullName": "Иванов Иван",
-        "avatarUrl": "https://randomuser.me/api/portraits/thumb/men/57.jpg",
         "birthdate": "1976-10-10",
-        "gender": "мужской",
+        "gender": "male",
         "address": "ул. Звенигороская, 47б",
         "email": "ivanov@mail.ru"
-    },
-    {
-        "id": 2,
+    },{
+        "id": 1,
         "fullName": "Петров Петр",
-        "avatarUrl": "https://randomuser.me/api/portraits/thumb/men/8.jpg",
         "birthdate": "1957-01-14",
-        "gender": "мужской",
+        "gender": "male",
         "address": "Пр. Науки, 13",
         "email": "petrov@mail.ru"
-    },
-    {
-        "id": 3,
+    },{
+        "id": 2,
         "fullName": "Натальина Наталья",
-        "avatarUrl": "https://randomuser.me/api/portraits/thumb/women/7.jpg",
         "birthdate": "1990-07-03",
-        "gender": "женский",
+        "gender": "female",
         "address": "ул. Феодоры, 59",
         "email": "natasha@mail.ru"
+    },{
+        "id": 3,
+        "fullName": "Bender Rodriguez",
+        "birthdate": "2996-01-01",
+        "gender": "male",
+        "address": "Brooklyn, New York",
+        "email": "bender@planet-express.com"
+    },{
+        "id": 4,
+        "fullName": "Luc Skywalker",
+        "birthdate": "3000-01-01",
+        "gender": "male",
+        "address": "Polis Massa, Tatooine",
+        "email": "luc@star-wars.com"
+    },{
+        "id": 5,
+        "fullName": "Philip Fry",
+        "birthdate": "1974-08-09",
+        "gender": "male",
+        "address": "Brooklyn, New York",
+        "email": "fry@planet-express.com"
+    },{
+        "id": 6,
+        "fullName": "Yoda Jedi Master",
+        "birthdate": "3000-01-01",
+        "gender": "male",
+        "address": "Somewhere in the Galaxy",
+        "email": "yoda@star-wars.com"
+    },{
+        "id": 7,
+        "fullName": "Darth Vader",
+        "birthdate": "3000-01-01",
+        "gender": "male",
+        "address": "Somewhere in the Galaxy",
+        "email": "vader@star-wars.com"
+    },{
+        "id": 8,
+        "fullName": "Princess Leia Organa",
+        "birthdate": "3000-01-01",
+        "gender": "female",
+        "address": "Alderaan",
+        "email": "leia@star-wars.com"
+    },{
+        "id": 9,
+        "fullName": "Professor Hubert Farnsworth",
+        "birthdate": "3000-01-01",
+        "gender": "male",
+        "address": "Brooklyn, New York",
+        "email": "hubert@planet-express.com"
     }
 ]`;
